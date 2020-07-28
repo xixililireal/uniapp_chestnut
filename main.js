7eec9795e061e0e8f5410d3eeff1ee4c522c7a22
xixililireal <45424526+xixililireal@users.noreply.github.com>
2020-07-28 11:22:20 +0800

    小栗子

---
+++ main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App'
+// import './static/css/public.css'
+import utils from "./utils/utils";
+
+Vue.prototype.$wxapp = utils;
+Vue.config.productionTip = false
+
+Vue.mixin({
+  methods:{
+    setTabBarIndex(index){
+      if (typeof this.$mp.page.getTabBar === 'function' &&
+        this.$mp.page.getTabBar()) {
+        this.$mp.page.getTabBar().setData({
+          selected:index
+        })
+      }
+    }
+  }
+})
+
+App.mpType = 'app'
+
+const app = new Vue({
+	...App
+})
+app.$mount()
